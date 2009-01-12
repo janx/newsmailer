0ffd80fc9a5d1810bd71624edffc3d6ad60040ee
Jan X <jan.h.xie@gmail.com>
2009-01-12 22:00:55 +0800

    test for multi thread db write

--- test/unit/crawler_test.rb
+++ test/unit/crawler_test.rb
@@ -9,5 +9,18 @@ class CrawlerTest < ActiveSupport::TestCase
     assert_equal 1, sliced[3].size
   end
 
+  test "concurrency save articles" do
+    threads = []
+    1.upto(5) { |i|
+      threads << Thread.new { 
+        sleep 3
+        1.upto(10) { |j|
+          Article.insert :title => "a_#{i}_#{j}", :content_type => 'text/html', :modified_at => Time.now, :url => "http://www.google.com/?x=#{i}&y=#{j}", :content => "#{i} #{j}", :feed_id => 1
+        }
+      }
+    }
+    threads.each {|t| t.join}
+  end
+
 end
 
