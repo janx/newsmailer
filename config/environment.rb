6ae0353472abf9d7ef198e2dac28cb2e45ee1e47
Jan X <jan.h.xie@gmail.com>
2009-05-18 10:47:37 +0800

    sqlite lib

--- config/environment.rb
+++ config/environment.rb
@@ -73,6 +73,7 @@ Rails::Initializer.run do |config|
   # Please note that observers generated using script/generate observer need to have an _observer suffix
   # config.active_record.observers = :cacher, :garbage_collector, :forum_observer
 
+  config.gem 'sqlite3-ruby'
   config.gem 'rfeedparser', :version => '>=0.9.951'
   config.gem 'thoughtbot-shoulda', :lib => 'shoulda', :version => '>=2.0.6', :source => "http://gems.github.com"
 end
